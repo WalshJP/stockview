e3e32a30482196313293095712d32f3ce0cac9d7
Jordan <jordan.walsh@mts.com>
2018-08-01 16:58:13 -0400

    adding search bar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,81 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class SearchBox extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {  
+    console.log('handling change...');   
+	
+    let value = e.target.value;
+    console.log('value: ' + value);
+	
+    this.props.onChange(value);
+  }
+  render() {
+    return (
+    <div className="SearchBox">
+    <input className="search" type="search" onChange={this.handleChange}/>
+    </div>
+    );
+  }
+}
+
+class SearchBoxContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+    };
+    this.updateValue = this.updateValue.bind(this);
+    this.runApi = this.runApi.bind(this);
+  }
+  
+  updateValue(newValue) {
+    console.log('updating value...');
+
+    this.setState({value: newValue});
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  runApi() {
+    console.log('running api...');
+
+  }    
+  
+  render() {
+    return (
+    <div>
+    <SearchBox onChange={this.updateValue}/>
+    </div>
+    )
+  }
+  
+}
+
+
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			
+		}
+		this.handleInfoUpdate = this.handleInfoUpdate.bind(this);
+	}
+
+	handleInfoUpdate() {
+
+	}
+	
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+		<SearchBoxContainer onUpdateInfo={this.handleInfoUpdate} />
       </div>
     );
   }
